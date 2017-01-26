78fc9aa2bfc9d34c3d29d738a8e157ff117c2675
Christian Ciach <christian.ciach@gmail.com>
2017-01-26 19:48:27 +0100

    set BOARD_KERNEL_SEPARATED_DT := false

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,7 +52,7 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_SEPARATED_DT := false
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x08000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/lenovo/msm8976
 TARGET_KERNEL_CONFIG := lineage_YTX703F_defconfig
