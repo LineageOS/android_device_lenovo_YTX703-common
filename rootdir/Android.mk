ac739731e5c0a0d7654d88e26d16eb0d71e82c9a
Matthias Lenk <matthias.lenk@gmx.de>
2017-01-29 22:51:47 +0100

    update proprietary files and enable SD card

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -49,4 +49,5 @@ LOCAL_MODULE		:= init.qcom.bt.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.bt.sh
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
