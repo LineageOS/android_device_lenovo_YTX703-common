ee47ac555a8895d07739dd72bcf796d458b7da6b
Matthias Lenk <matthias.lenk@gmx.de>
2017-02-02 17:40:49 +0100

    update for new proprietary blobs. Note some files are missing in proprietary-files.txt

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -49,5 +49,4 @@ LOCAL_MODULE		:= init.qcom.bt.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.bt.sh
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
