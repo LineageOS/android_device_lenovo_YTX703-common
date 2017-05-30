acf49849431a7533087a703229f93c701ace79ea
Vladimir Oltean <olteanv@gmail.com>
2017-05-30 11:03:00 +0300

    BoardConfigCommon: remove kernel config (set individually per device)
    Change-Id: I21c6c0a94ae7bdf5c43109e411386bb42cfd7909
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,7 +52,6 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x08000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/lenovo/msm8976
-TARGET_KERNEL_CONFIG := lineage_YTX703F_defconfig
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
