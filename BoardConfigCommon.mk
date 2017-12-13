7a35021e79853d90258da6a5a6f8620335df2bf9
Christian Ciach <christian.ciach@gmail.com>
2017-12-13 18:12:42 +0100

    corrected kernel defconfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,7 +52,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x08000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/lenovo/msm8976
-TARGET_KERNEL_CONFIG := YTX703_defconfig
+TARGET_KERNEL_CONFIG := lineage_YTX703_defconfig
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
