cd1153361e4575277340354aea456f08105731e0
Christian Ciach <christian.ciach@gmail.com>
2017-12-31 19:04:09 +0100

    use stock audio_policy_configuration.xml instead of deprecated .conf file

--- msm8976.mk
+++ msm8976.mk
@@ -90,7 +90,12 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml     
     $(LOCAL_PATH)/audio/ftm_test_config:system/etc/ftm_test_config \
     $(LOCAL_PATH)/audio/ftm_test_config_msm8952-skum-snd-card:system/etc/ftm_test_config_msm8952-skum-snd-card \
     $(LOCAL_PATH)/audio/ftm_test_config_msm8976-skun-snd-card:system/etc/ftm_test_config_msm8976-skun-snd-card \
