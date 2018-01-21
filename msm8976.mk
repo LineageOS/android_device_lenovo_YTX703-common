f699008590e02409788fbb770e9519197f3a714d
Christian Ciach <christian.ciach@gmail.com>
2018-01-21 15:09:29 +0100

    do not build gps from source, use blobs instead
    This may fix crashes of the android system (SEGFAULTS)

--- msm8976.mk
+++ msm8976.mk
@@ -125,8 +125,8 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
-    $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml     
-    
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
@@ -147,16 +147,20 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
+    FM2 \
+    FMRecord \
+    libfmjni \
+    libqcomfm_jni \
+    qcom.fmradio
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
 
 # GPS
+# (gps.default from vendor tree)
 PRODUCT_PACKAGES += \
-    gps.msm8952 \
+    gps.default \
     libcurl
 
 PRODUCT_PACKAGES += \
