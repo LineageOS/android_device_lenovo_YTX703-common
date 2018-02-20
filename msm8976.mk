3eca9fa02717ce76301d4816968956b55875ec19
Vladimir Oltean <olteanv@gmail.com>
2018-02-20 11:55:00 +0200

    build data-ipa-cfg-mgr from source (libipanat, ipacm)
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- msm8976.mk
+++ msm8976.mk
@@ -262,6 +262,12 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# data-ipa-cfg-mgr
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    libipanat
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
