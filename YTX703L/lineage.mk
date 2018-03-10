5b53520bb37852c735705c4160bb51961db41ab0
Vladimir Oltean <olteanv@gmail.com>
2018-03-10 04:27:42 +0200

    commonize: lineage.mk: move each product makefile to its own folder
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- YTX703L/lineage.mk
+++ YTX703L/lineage.mk
@@ -1,21 +1,21 @@
 # This product makefile is parsed during the lunch command,
 # which takes it from vendorsetup.sh
 
-$(call inherit-product, device/lenovo/YTX703/YTX703F.mk)
+$(call inherit-product, device/lenovo/YTX703-common/YTX703L/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_YTX703F
+PRODUCT_NAME := lineage_YTX703L
 PRODUCT_BRAND := Lenovo
 PRODUCT_MANUFACTURER := LENOVO
-PRODUCT_DEVICE := YTX703F
-PRODUCT_MODEL := Lenovo Yoga Tab 3 Plus Wi-Fi
+PRODUCT_DEVICE := YTX703L
+PRODUCT_MODEL := Lenovo Yoga Tab 3 Plus LTE
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
 
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=YT-X703F \
-    BUILD_FINGERPRINT=Lenovo/YT-X703F/YT-X703F:6.0.1/S100/YT-X703F_S000725_161230_ROW:user/release-keys \
+    PRODUCT_NAME=YT-X703L \
+    BUILD_FINGERPRINT=Lenovo/YT-X703L/YT-X703L:6.0.1/S100/YT-X703L_S000734_170307_ROW:user/release-keys \
     PRIVATE_BUILD_DESC="msm8952_64-user 6.0.1 MMB29M eng.maojialu.20161230.160517 release-keys"
