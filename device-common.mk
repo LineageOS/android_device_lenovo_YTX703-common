348dc9320e624dd0b9791966ea03400ef9b85ef5
Vladimir Oltean <olteanv@gmail.com>
2018-03-11 03:15:19 +0200

    Revert "do not build gps from source, use blobs instead"
    This reverts commit f699008590e02409788fbb770e9519197f3a714d.

--- device-common.mk
+++ device-common.mk
@@ -125,8 +125,8 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
-    $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
-
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml     
+    
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
@@ -150,9 +150,8 @@ PRODUCT_PACKAGES += \
     fs_config_files
 
 # GPS
-# (gps.default from vendor tree)
 PRODUCT_PACKAGES += \
-    gps.default \
+    gps.msm8952 \
     libcurl
 
 PRODUCT_PACKAGES += \
