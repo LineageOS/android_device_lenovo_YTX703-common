707a154cc56e23d042273144d49f8a7b861d0e5a
Vladimir Oltean <olteanv@gmail.com>
2018-03-19 00:38:51 +0200

    gps: import izat.conf from stock
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- gps/etc/Android.mk
+++ gps/etc/Android.mk
@@ -10,3 +10,13 @@ LOCAL_SRC_FILES := gps.conf
 
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := izat.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/
+LOCAL_SRC_FILES := izat.conf
+
+include $(BUILD_PREBUILT)
+
