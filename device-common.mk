75e28c9da0b2e459638dea9d3c82ea95113510af
Vladimir Oltean <olteanv@gmail.com>
2018-03-27 03:41:32 +0300

    refactoring: move "keylayout" to configs/ directory
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -152,12 +152,8 @@ PRODUCT_PACKAGES += \
     libebtc
 
 # Keylayout
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
+PRODUCT_COPY_FILES += $(foreach keylayout_config, $(wildcard $(LOCAL_PATH)/configs/keylayout/*), \
+    $(keylayout_config):$(addprefix system/usr/keylayout/, $(notdir $(keylayout_config))) )
 
 # Lights
 PRODUCT_PACKAGES += \
