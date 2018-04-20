4bcfdae380cbb50411672b0757001d92a1c63b08
Vladimir Oltean <olteanv@gmail.com>
2018-04-20 10:40:45 +0300

    rootdir: remove init.qcom.console.sh
    * was used only for YTX703L anyway, to start an inexistent "console"
      service
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -62,14 +62,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.console.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.console.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.radio.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
