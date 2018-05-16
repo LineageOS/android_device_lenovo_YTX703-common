adc1f0ea6d67b1771bf37a80b0499622b4dfcdc5
Quallenauge <Hamsi2k@freenet.de>
2018-05-16 22:14:14 +0200

    lights: Update lights to binder HAL.
    Adapted from https://github.com/sonyxperiadev/device-sony-common/tree/master/hardware/liblights .
    
    Adapt light implementation to lenovo specifics.
    Define dedicated power sysfs files - we only have one color.
    Use alpha channel for brightness calculation.
    Use seconds instead of micro seconds for blink pattern.
    
    The alpha channel is only 0 if the lights are set to off.
    When changing the notification settings, the led brightness is set 0%
    the alpha channel is 1 which breaks the turn off detection.
    So simple decrease the brightness by 1.
    This has sideeffects to the blink mode, which is also adapted with this commit.
    
    This device has only one LED path and not multiple (as the sony devices,
    where this template comes from).
    
    Change-Id: I4fa4b4c5c384efec29a45ae251ee4407c16940d2

---
+++ hardware/liblight/Light.h
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2018 Shane Francis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef ANDROID_HARDWARE_LIGHT_V2_0_LIGHT_H
+#define ANDROID_HARDWARE_LIGHT_V2_0_LIGHT_H
+
+#include <android/hardware/light/2.0/ILight.h>
+#include <hardware/hardware.h>
+#include <hardware/lights.h>
+#include <hidl/Status.h>
+#include <hidl/MQDescriptor.h>
+#include <map>
+
+#include <cutils/properties.h>
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+#include <linux/msm_mdp.h>
+
+#include <sys/ioctl.h>
+#include <sys/types.h>
+
+#include <hardware/lights.h>
+
+#define DEFAULT_LOW_PERSISTENCE_MODE_BRIGHTNESS 255
+
+#define LP_MODE_BRIGHTNESS_PROPERTY "sys.display.low_persistence_mode_brightness"
+
+struct lights_t {
+    pthread_mutex_t g_lock;
+    pthread_mutex_t g_lcd_lock;
+    int g_last_backlight_mode;
+    int backlight_bits;
+};
+
+static constexpr int RAMP_SIZE = 8;
+static constexpr int BRIGHTNESS_RAMP[RAMP_SIZE] = {0, 14, 28, 42, 56, 70, 84, 100};
+
+char const*const POWER_BLINK_FILE
+        = "/sys/class/leds/green/blink";
+
+char const*const POWER_LED_FILE
+        = "/sys/class/leds/green/brightness";
+
+char const*const POWER_PATTERN_FILE
+        = "/sys/class/leds/green/led_time";
+
+const static std::string LCD_FILE
+        = "/sys/class/backlight/lcd-bl/brightness";
+
+const static std::string LCD_MAX_FILE
+        = "/sys/class/backlight/lcd-bl/max_brightness";
+
+namespace android {
+    namespace hardware {
+        namespace light {
+            namespace V2_0 {
+                namespace implementation {
+                    using::android::hardware::light::V2_0::ILight;
+                    using::android::hardware::light::V2_0::LightState;
+                    using::android::hardware::light::V2_0::Status;
+                    using::android::hardware::light::V2_0::Type;
+                    using::android::hardware::Return;
+                    using::android::hardware::Void;
+                    using::android::hardware::hidl_vec;
+                    using::android::hardware::hidl_string;
+                    using::android::sp;
+
+                    struct Light : public ILight {
+                        public:
+                        Light();
+
+                        static ILight *getInstance();
+
+                        // Methods from ::android::hardware::light::V2_0::ILight follow.
+                        Return <Status> setLight(Type type, const LightState &state) override;
+                        Return<void> getSupportedTypes(getSupportedTypes_cb _hidl_cb) override;
+
+                        private:
+                        static Light *sInstance;
+                        lights_t *mDevice;
+                        LightState batteryState;
+                        LightState notificationState;
+                        int setLightBacklight(const LightState &state);
+                        int setLightBattery(const LightState &state);
+                        int setLightNotifications(const LightState &state);
+                        void handleSpeakerBatteryLocked();
+                        int setSpeakerLightLocked(const LightState &state);
+                        std::string getScaledDutyPcts(int brightness);
+                        int isLit(const LightState &state);
+                        bool isRgbSyncAvailable();
+                        int rgbToBrightness(const LightState &state);
+                        static int writeInt(const std::string &path, int value);
+                        static int readInt(const std::string &path);
+                        static int writeStr(const std::string &path, const std::string &value);
+                        void openHal();
+                    };
+                }  // namespace implementation
+            }  // namespace V2_0
+        }  // namespace light
+    }  // namespace hardware
+}  // namespace android
+
+#endif  // ANDROID_HARDWARE_LIGHT_V2_0_LIGHT_H
