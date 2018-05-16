b121dc3ce56206cc6368e3ac93d44bbddc40f876
Quallenauge <Hamsi2k@freenet.de>
2018-05-16 22:28:49 +0200

    QCamera2: Increase camera daemon start timeout
    Issue: Camera is not launching for the first time as camera daemon is not up.
    Change-Id: Ieadffb6b9859a0e4b7ad43c56a9dd6360d01343b

--- hardware/camera/QCamera2/stack/mm-camera-interface/src/mm_camera_interface.c
+++ hardware/camera/QCamera2/stack/mm-camera-interface/src/mm_camera_interface.c
@@ -40,6 +40,8 @@
 #include <cutils/properties.h>
 #include <stdlib.h>
 
+#define EXTRA_ENTRY 6
+
 #include "mm_camera_dbg.h"
 #include "mm_camera_interface.h"
 #include "mm_camera_sock.h"
@@ -1770,6 +1772,7 @@ void sort_camera_info(int num_cam)
 uint8_t get_num_of_cameras()
 {
     int rc = 0;
+    int i = 0;
     int dev_fd = -1;
     struct media_device_info mdev_info;
     int num_media_devices = 0;
@@ -1864,7 +1867,15 @@ uint8_t get_num_of_cameras()
     cfg.cfgtype = CFG_SINIT_PROBE_WAIT_DONE;
     cfg.cfg.setting = NULL;
     if (ioctl(sd_fd, VIDIOC_MSM_SENSOR_INIT_CFG, &cfg) < 0) {
-        CDBG_ERROR("failed");
+        ALOGI("failed...Camera Daemon may not up so try again");
+        for(i = 0; i < (MM_CAMERA_EVT_ENTRY_MAX + EXTRA_ENTRY); i++) {
+            if (ioctl(sd_fd, VIDIOC_MSM_SENSOR_INIT_CFG, &cfg) < 0) {
+                ALOGI("failed...Camera Daemon may not up so try again");
+                continue;
+            }
+            else
+                break;
+        }
     }
     close(sd_fd);
     dev_fd = -1;
