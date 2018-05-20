30de60c36234c3a61a0ed9ef95d962643a29432c
Vladimir Oltean <olteanv@gmail.com>
2018-05-21 00:39:35 +0300

    device-common: fix init.qcom.post_boot.sh not being copied to ramdisk
    * The situation with rootdir files needing to be referenced in 2 places
      creates a lot of chaos with my negligence. I'm going to do something
      drastic about it.
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -125,11 +125,11 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.qcom.rc \
-    init.qcom.sh \
-    init.target.rc \
     init.qcom.bt.sh \
+    init.qcom.post_boot.sh \
     init.qcom.power.rc \
+    init.qcom.radio.sh \
+    init.qcom.rc \
     init.qcom.usb.rc \
     ueventd.qcom.rc \
 
