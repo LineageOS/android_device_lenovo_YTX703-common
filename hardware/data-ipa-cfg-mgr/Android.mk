4b9d04840fa1755c4a40e1d10ea532e2d947fea5
Quallenauge <Hamsi2k@freenet.de>
2018-05-30 00:30:01 -0100

    ipa: Add proper Android.mk to build components.
    It is including makefiles for hal, ipacm, and ipanat folders.
    
    Change-Id: If457e40beabc15701f8a4f4d51f2de1a23953f83

---
+++ hardware/data-ipa-cfg-mgr/Android.mk
@@ -0,0 +1,6 @@
+IPA_LOCAL_PATH := $(call my-dir)
+
+include $(IPA_LOCAL_PATH)/hal/Android.mk
+include $(IPA_LOCAL_PATH)/ipacm/src/Android.mk
+include $(IPA_LOCAL_PATH)/ipanat/src/Android.mk
+include $(IPA_LOCAL_PATH)/ipanat/test/Android.mk
