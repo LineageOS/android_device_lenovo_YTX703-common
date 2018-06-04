d93137778be2249bf20b56d40c2a2f5062d8c6a2
Vladimir Oltean <olteanv@gmail.com>
2018-06-04 22:46:58 +0300

    extract_utils: adapt to cm -> lineage name change
    Change-Id: I2cc9ebc3b8ccdb869de1ae1f25b0597aa1b4eef4
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -21,9 +21,9 @@ set -e
 MY_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -52,25 +52,25 @@ fi
 
 if [ -n "$SETUP" ]; then
     # Initialize the helper for common
-    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true false
+    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true false
     "$MY_DIR"/setup-makefiles.sh false
 
     if [ -s "$MY_DIR"/$DEVICE/proprietary-files.txt ]; then
         # Initalize the helper for device
-        (setup_vendor "$DEVICE" "$VENDOR/$DEVICE_COMMON" "$CM_ROOT" false false)
+        (setup_vendor "$DEVICE" "$VENDOR/$DEVICE_COMMON" "$LINEAGE_ROOT" false false)
         "$MY_DIR"/setup-makefiles.sh false
     fi
 else
     # Initialize the helper for common
     (
-    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true "$CLEANUP"
+    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEANUP"
     extract "$MY_DIR"/proprietary-files.txt "$SRC"
     )
 
     if [ -s "$MY_DIR"/$DEVICE/proprietary-files.txt ]; then
         # Reinitialize the helper for device
         (
-        setup_vendor "$DEVICE" "$VENDOR/$DEVICE_COMMON" "$CM_ROOT" false "$CLEANUP"
+        setup_vendor "$DEVICE" "$VENDOR/$DEVICE_COMMON" "$LINEAGE_ROOT" false "$CLEANUP"
         extract "$MY_DIR"/$DEVICE/proprietary-files.txt "$SRC"
         )
     fi
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -21,10 +21,10 @@ set -e
 MY_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 CLEANUP=$1
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -32,7 +32,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" "true" "$CLEANUP"
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" "true" "$CLEANUP"
 
 # Copyright headers and guards
 write_headers "YTX703F YTX703L"
@@ -45,7 +45,7 @@ write_footers
 
 if [ -s "$MY_DIR"/$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR/$DEVICE_COMMON" "$CM_ROOT" "false" "$CLEANUP"
+    setup_vendor "$DEVICE" "$VENDOR/$DEVICE_COMMON" "$LINEAGE_ROOT" "false" "$CLEANUP"
 
     # Copyright headers and guards
     write_headers
