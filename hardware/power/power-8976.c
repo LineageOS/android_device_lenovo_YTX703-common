88093ce005dd460bd8f8212acf8a64239347333b
dianlujitao <dianlujitao@lineageos.org>
2018-06-23 00:23:03 +0200

    power: Support VR + Sustained performance mode.
    Adapted from msm8953 power HAL.
    
    Change-Id: I92dc1f2a5f3b567821a6af2a466551bcae4352b1

--- hardware/power/power-8976.c
+++ hardware/power/power-8976.c
@@ -49,7 +49,8 @@
 #include "performance.h"
 #include "power-common.h"
 
-static int video_encode_hint_sent;
+#define CHECK_HANDLE(x) ((x)>0)
+#define NUM_PERF_MODES  3
 
 static int current_power_profile = PROFILE_BALANCED;
 
@@ -119,59 +120,141 @@ static void set_power_profile(int profile)
     current_power_profile = profile;
 }
 
-static void process_video_encode_hint(void *metadata)
+typedef enum {
+    NORMAL_MODE       = 0,
+    SUSTAINED_MODE    = 1,
+    VR_MODE           = 2,
+    VR_SUSTAINED_MODE = (SUSTAINED_MODE|VR_MODE),
+    INVALID_MODE      = 0xFF
+} perf_mode_type_t;
+
+typedef struct perf_mode {
+    perf_mode_type_t type;
+    int perf_hint_id;
+} perf_mode_t;
+
+perf_mode_t perf_modes[NUM_PERF_MODES] = {
+    { SUSTAINED_MODE, SUSTAINED_PERF_HINT },
+    { VR_MODE, VR_MODE_HINT },
+    { VR_SUSTAINED_MODE, VR_MODE_SUSTAINED_PERF_HINT }
+};
+
+static int current_mode = NORMAL_MODE;
+
+static inline int get_perfd_hint_id(perf_mode_type_t type) {
+    int i;
+    for (i = 0; i < NUM_PERF_MODES; i++) {
+        if (perf_modes[i].type == type) {
+            ALOGD("Hint id is 0x%x for mode 0x%x", perf_modes[i].perf_hint_id, type);
+            return perf_modes[i].perf_hint_id;
+        }
+    }
+    ALOGD("Couldn't find the hint for mode 0x%x", type);
+    return 0;
+}
+
+static int switch_mode(perf_mode_type_t mode) {
+    int hint_id = 0;
+    static int perfd_mode_handle = -1;
+
+    // release existing mode if any
+    if (CHECK_HANDLE(perfd_mode_handle)) {
+        ALOGD("Releasing handle 0x%x", perfd_mode_handle);
+        release_request(perfd_mode_handle);
+        perfd_mode_handle = -1;
+    }
+    // switch to a perf mode
+    hint_id = get_perfd_hint_id(mode);
+    if (hint_id != 0) {
+        perfd_mode_handle = perf_hint_enable(hint_id, 0);
+        if (!CHECK_HANDLE(perfd_mode_handle)) {
+            ALOGE("Failed perf_hint_interaction for mode: 0x%x", mode);
+            return -1;
+        }
+        ALOGD("Acquired handle 0x%x", perfd_mode_handle);
+    }
+    return 0;
+}
+
+static int process_perf_hint(void *data, perf_mode_type_t mode) {
+    // enable
+    if (*(int32_t *)data) {
+        ALOGI("Enable request for mode: 0x%x", mode);
+        // check if mode is current mode
+        if (current_mode & mode) {
+            ALOGD("Mode 0x%x already enabled", mode);
+            return HINT_HANDLED;
+        }
+        // enable requested mode
+        if (0 != switch_mode(current_mode | mode)) {
+            ALOGE("Couldn't enable mode 0x%x", mode);
+            return HINT_NONE;
+        }
+        current_mode |= mode;
+        ALOGI("Current mode is 0x%x", current_mode);
+    // disable
+    } else {
+        ALOGI("Disable request for mode: 0x%x", mode);
+        // check if mode is enabled
+        if (!(current_mode & mode)) {
+            ALOGD("Mode 0x%x already disabled", mode);
+            return HINT_HANDLED;
+        }
+        // disable requested mode
+        if (0 != switch_mode(current_mode & ~mode)) {
+            ALOGE("Couldn't disable mode 0x%x", mode);
+            return HINT_NONE;
+        }
+        current_mode &= ~mode;
+        ALOGI("Current mode is 0x%x", current_mode);
+    }
+
+    return HINT_HANDLED;
+}
+
+static int process_video_encode_hint(void *metadata)
 {
     char governor[80];
     struct video_encode_metadata_t video_encode_metadata;
+    static int video_encode_handle = 0;
+
+    if (!metadata) {
+        return HINT_NONE;
+    }
 
     if (get_scaling_governor_check_cores(governor, sizeof(governor), CPU0) == -1) {
         if (get_scaling_governor_check_cores(governor, sizeof(governor), CPU1) == -1) {
             if (get_scaling_governor_check_cores(governor, sizeof(governor), CPU2) == -1) {
                 if (get_scaling_governor_check_cores(governor, sizeof(governor), CPU3) == -1) {
                     ALOGE("Can't obtain scaling governor.");
-                    return;
+                    return HINT_NONE;
                 }
             }
         }
     }
 
-    if (!metadata) {
-        return;
-    }
-
-    /* Initialize encode metadata struct fields. */
+    /* Initialize encode metadata struct fields */
     memset(&video_encode_metadata, 0, sizeof(struct video_encode_metadata_t));
     video_encode_metadata.state = -1;
-    video_encode_metadata.hint_id = DEFAULT_VIDEO_ENCODE_HINT_ID;
 
-    if (parse_video_encode_metadata((char *)metadata,
-            &video_encode_metadata) == -1) {
+    if (parse_video_encode_metadata((char *)metadata, &video_encode_metadata) == -1) {
         ALOGE("Error occurred while parsing metadata.");
-        return;
+        return HINT_NONE;
     }
 
     if (video_encode_metadata.state == 1) {
         if (is_interactive_governor(governor)) {
-            int resource_values[] = {
-                INT_OP_CLUSTER0_USE_SCHED_LOAD, 0x1,
-                INT_OP_CLUSTER1_USE_SCHED_LOAD, 0x1,
-                INT_OP_CLUSTER0_USE_MIGRATION_NOTIF, 0x1,
-                INT_OP_CLUSTER1_USE_MIGRATION_NOTIF, 0x1,
-                INT_OP_CLUSTER0_TIMER_RATE, BIG_LITTLE_TR_MS_40,
-                INT_OP_CLUSTER1_TIMER_RATE, BIG_LITTLE_TR_MS_40
-            };
-            if (!video_encode_hint_sent) {
-                perform_hint_action(video_encode_metadata.hint_id,
-                        resource_values, ARRAY_SIZE(resource_values));
-                video_encode_hint_sent = 1;
-            }
+            video_encode_handle = perf_hint_enable(
+                    VIDEO_ENCODE_HINT, 0);
+            return HINT_HANDLED;
         }
     } else if (video_encode_metadata.state == 0) {
         if (is_interactive_governor(governor)) {
-            undo_hint_action(video_encode_metadata.hint_id);
-            video_encode_hint_sent = 0;
+            release_request(video_encode_handle);
+            return HINT_HANDLED;
         }
     }
+    return HINT_NONE;
 }
 
 int power_hint_override(power_hint_t hint, void *data)
@@ -189,8 +272,11 @@ int power_hint_override(power_hint_t hint, void *data)
 
     switch (hint) {
         case POWER_HINT_VIDEO_ENCODE:
-            process_video_encode_hint(data);
-            return HINT_HANDLED;
+            return process_video_encode_hint(data);
+        case POWER_HINT_SUSTAINED_PERFORMANCE:
+            return process_perf_hint(data, SUSTAINED_MODE);
+        case POWER_HINT_VR_MODE:
+            return process_perf_hint(data, VR_MODE);
         case POWER_HINT_INTERACTION:
         {
             int resource_values[] = {
