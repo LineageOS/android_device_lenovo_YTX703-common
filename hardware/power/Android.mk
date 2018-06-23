e46be63099426f23c8990803106c6a0d84f83759
Quallenauge <Hamsi2k@freenet.de>
2018-06-23 20:36:43 +0200

    power: Handle TARGET_USES_INTERACTION_BOOST flag.
    This enables support for power profiles.
    It is not set hardcoded within the power hal library,
    to let it compareable to the lineage upstream implementation.
    
    Change-Id: I17fcd91e4c353735889cc9afa27edd92ee61d576

--- hardware/power/Android.mk
+++ hardware/power/Android.mk
@@ -1,8 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_C_INCLUDES := hardware/qcom/power
 LOCAL_CFLAGS := -Wall -Werror
+
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
 LOCAL_SRC_FILES := power-8976.c
 LOCAL_MODULE := libpower_8976
 include $(BUILD_STATIC_LIBRARY)
