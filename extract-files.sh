0db484ca296b147c0b325856c6d6d1656edb389f
Vladimir Oltean <olteanv@gmail.com>
2018-07-03 01:44:52 +0300

    extract_utils: drop -s (setup) feature
    * This feature creates board proprietary makefiles without the
      blob extraction process. The same thing can be accomplished by running
      with -d from a fake location.
    * I need to drop support for this because it complicates the logic of a
      future commit that adds support for proprietary-files-twrp.txt, for no
      good reason.
    
    Change-Id: Ib3e9959362913d2eb807a50033685002994e77f7
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -30,17 +30,15 @@ if [ ! -f "${HELPER}" ]; then
 fi
 . "${HELPER}"
 
-while getopts ":nhsd:" options
+while getopts ":nhd:" options
 do
 	case $options in
 	n ) CLEANUP="false" ;;
 	d ) SRC=$OPTARG ;;
-	s ) SETUP=1 ;;
 	x ) DEVICE_ONLY=1 ;;
 	h ) echo "Usage: `basename $0` [OPTIONS] "
 	    echo "  -n  No cleanup"
 	    echo "  -d  Fetch blob from filesystem"
-	    echo "  -s  Setup only, no extraction"
 	    echo "  -x  Only device specific extraction"
 	    echo "  -h  Show this help"
 	    exit ;;
@@ -52,32 +50,20 @@ if [ -z $SRC ]; then
 	SRC=adb
 fi
 
-if [ -n "${SETUP}" ]; then
-	# Initialize the helper for common
-	setup_vendor "${DEVICE_COMMON}" "$VENDOR" "${LINEAGE_ROOT}" true false
-	"${MY_DIR}/setup-makefiles.sh" false
-	
-	if [ -s "${MY_DIR}/${DEVICE}/proprietary-files.txt" ]; then
-		# Initalize the helper for device
-		(setup_vendor "${DEVICE}" "${VENDOR}/${DEVICE_COMMON}" "${LINEAGE_ROOT}" false false)
-		"${MY_DIR}/setup-makefiles.sh" false
-	fi
-else
-	# Initialize the helper for YTX703-common
-	if [ -z "${DEVICE_ONLY}" ]; then
+# Initialize the helper for YTX703-common
+if [ -z "${DEVICE_ONLY}" ]; then
 	(
 	setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEANUP}"
 	extract "${MY_DIR}/proprietary-files.txt" "${SRC}"
 	)
-	fi
-	
-	if [ -s "${MY_DIR}/${DEVICE}/proprietary-files.txt" ]; then
-		# Reinitialize the helper for YTX703-common/${device}
-		(
-		setup_vendor "${DEVICE}" "${VENDOR}/${DEVICE_COMMON}" "${LINEAGE_ROOT}" false "${CLEANUP}"
-		extract "${MY_DIR}/${DEVICE}/proprietary-files.txt" "${SRC}"
-		)
-	fi
-	
-	"${MY_DIR}/setup-makefiles.sh" "${CLEANUP}"
 fi
+	
+if [ -s "${MY_DIR}/${DEVICE}/proprietary-files.txt" ]; then
+	# Reinitialize the helper for YTX703-common/${device}
+	(
+	setup_vendor "${DEVICE}" "${VENDOR}/${DEVICE_COMMON}" "${LINEAGE_ROOT}" false "${CLEANUP}"
+	extract "${MY_DIR}/${DEVICE}/proprietary-files.txt" "${SRC}"
+	)
+fi
+
+"${MY_DIR}/setup-makefiles.sh" "${CLEANUP}"
