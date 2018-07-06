f6e983df7f23e0273e7d523e82673aee95afa675
Quallenauge <Hamsi2k@freenet.de>
2018-07-06 01:55:01 -0100

    power: Build power helper lib with the VNDK.
    Add needed libraries and mark module as
    LOCAL_VENDOR_MODULE := true
    
    Test: BOARD_VNDK_VERSION=current mka android.hardware.power@1.0-service-qti
    Change-Id: Ia4d5f045fbd5d1dd11cd0c7a71c8c4e72d285eed

--- hardware/power/Android.mk
+++ hardware/power/Android.mk
@@ -4,6 +4,8 @@ include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := hardware/qcom/power
 LOCAL_CFLAGS := -Wall -Werror
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_HEADER_LIBRARIES += libhardware_headers
 
 ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
     LOCAL_CFLAGS += -DINTERACTION_BOOST
@@ -11,4 +13,5 @@ endif
 
 LOCAL_SRC_FILES := power-8976.c
 LOCAL_MODULE := libpower_8976
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_STATIC_LIBRARY)
