4841eeabbdafac9f97bdbe5b3c6585bbfad2a927
Quallenauge <Hamsi2k@freenet.de>
2018-09-14 20:20:38 +0200

    fstab: Mount /firmware /persists ... to /mnt/vendor/...
    As new recommendation, folders on root should be avoided:
    https://source.android.com/devices/bootloader/system-as-root#device-specific-folders
    
    This is the first approach to mount /firmware, /persist, /dsp
    at /mnt/vendor.
    There are bind mounts created for bring up P compatibility.
    It has to be checked if they aren't needed.
    
    Change-Id: I89c638e5f52cc93b3195dd2694dc3c4cacf77237

--- config.fs
+++ config.fs
@@ -17,18 +17,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
-[dsp/]
-mode: 0771
-user: AID_MEDIA
-group: AID_MEDIA
-caps: 0
-
-[firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
 [persist/]
 mode: 0771
 user: AID_SYSTEM
