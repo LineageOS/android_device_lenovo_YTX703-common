6e5377ebd2d91af5eb8fb8943050e7498605290a
Quallenauge <Hamsi2k@freenet.de>
2018-09-25 01:53:07 +0300

    vintf: only include qti variant of gnss HAL
    Don't include the AOSP HAL which interferes with the qti version.
    If both hals are used init complains about duplicate definitions:
    init: /vendor/etc/init/android.hardware.gnss@1.0-service.rc: 1: ignored duplicate definition of service 'gnss_service'
    It is only by luck that the qti version is chosen and started by init.
    
    Change-Id: I9af6bffc1a99a4236c21d455cf9ddbf3d35020e6

--- common-treble.mk
+++ common-treble.mk
@@ -47,8 +47,8 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service
+    android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-service-qti
 
 # Light
 PRODUCT_PACKAGES += \
