192aa6713626a388f39bde1e4df89013cdcc8f85
Vladimir Oltean <olteanv@gmail.com>
2018-12-27 05:11:19 +0200

    proprietary-files: introduce fixup scripts and kang mode
    Also do the following:
    * Fix passing of arguments longer than one word, such as --section "Hello world"
    * Update the hashes and fixups for blobs kanged from bardock
    
    Change-Id: I917ed382a8c0e33ba7709151f2743a723dc50617
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- YTX703L/extract-files.sh
+++ YTX703L/extract-files.sh
@@ -15,6 +15,33 @@
 # limitations under the License.
 #
 
+# Sourced by the common device repo when extracting device-specific blobs
+function blob_fixup() {
+	case "${1}" in
+	vendor/bin/netmgrd)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		# Replace AID 2950 with 2901 (see commit eb1abadf)
+		sed -i -e "s|\xBC\x0B\x00\x00\xBB\x0B\x00\x00\x86\x0B\x00\x00\xE8\x03\x00\x00|\xBC\x0B\x00\x00\x55\x0B\x00\x00\x86\x0B\x00\x00\xE8\x03\x00\x00|g" "${2}"
+		;;
+	vendor/bin/qmuxd)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	vendor/lib/libdsi_netctrl.so | vendor/lib64/libdsi_netctrl.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	vendor/lib/libril-qc-qmi-1.so | vendor/lib64/libril-qc-qmi-1.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	esac
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+	return
+fi
+
+# Normal execution path when ran interactively from shell
 set -e -u -o pipefail
 
 # Required!
@@ -24,4 +51,4 @@ export VENDOR=lenovo
 
 MY_DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 
-${MY_DIR}/../extract-files.sh $@
+${MY_DIR}/../extract-files.sh "$@"
