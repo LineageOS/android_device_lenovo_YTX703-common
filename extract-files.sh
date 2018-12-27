67788b0fe600546ec044c04de2fc7dbaa5b6c893
Vladimir Oltean <olteanv@gmail.com>
2018-12-27 05:11:19 +0200

    proprietary-files: introduce fixup scripts and kang mode
    Also do the following:
    * Fix passing of arguments longer than one word, such as --section "Hello world"
    * Update the hashes and fixups for blobs kanged from bardock
    
    Change-Id: I917ed382a8c0e33ba7709151f2743a723dc50617
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -26,48 +26,213 @@ if [ ! -f "${HELPER}" ]; then
 	echo "Unable to find helper script at $HELPER"
 	exit 1
 fi
-. "${HELPER}"
+source "${HELPER}"
 
-while getopts ":nd:xh" options
-do
-	case $options in
-	n ) CLEANUP="false" ;;
-	d ) SRC=$OPTARG ;;
-	x ) DEVICE_ONLY=1 ;;
-	h ) echo "Usage: `basename $0` [OPTIONS] "
-	    echo "  -n  No cleanup"
-	    echo "  -d  Fetch blob from filesystem"
-	    echo "  -x  Only device specific extraction"
-	    echo "  -h  Show this help"
-	    exit ;;
-	* ) ;;
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+KANG=
+
+while [ "$#" -gt 0 ]; do
+	case "$1" in
+	-n|--no-cleanup)
+		CLEAN_VENDOR=false
+		;;
+	-k|--kang)
+		KANG="--kang"
+		;;
+	-s|--section)
+		SECTION="$2"; shift
+		CLEAN_VENDOR=false
+		;;
+	*)
+		SRC="$1"
+		;;
 	esac
+	shift
 done
 
-if [ -z $SRC ]; then
+if [ -z "$SRC" ]; then
 	SRC=adb
 fi
 
-# Initialize the helper for YTX703-common
-if [ -z "${DEVICE_ONLY}" ]; then
-	(
-	setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEANUP}"
-	extract "${MY_DIR}/proprietary-files.txt" "${SRC}"
-	if [ -s "${MY_DIR}/proprietary-files-twrp.txt" ]; then
-		extract "${MY_DIR}/proprietary-files-twrp.txt" "${SRC}"
-	fi
-	)
-fi
-	
-if [ -s "${MY_DIR}/${DEVICE}/proprietary-files.txt" ]; then
-	# Reinitialize the helper for YTX703-common/${device}
-	(
-	setup_vendor "${DEVICE}" "${VENDOR}/${DEVICE_COMMON}" "${LINEAGE_ROOT}" false "${CLEANUP}"
-	extract "${MY_DIR}/${DEVICE}/proprietary-files.txt" "${SRC}"
-	if [ -s "${MY_DIR}/${DEVICE}/proprietary-files-twrp.txt" ]; then
-		extract "${MY_DIR}/${DEVICE}/proprietary-files-twrp.txt" "$SRC"
-	fi
-	)
-fi
+shopt -s extglob
 
-"${MY_DIR}/setup-makefiles.sh" "${CLEANUP}"
+function blob_fixup() {
+	case "${1}" in
+	vendor/bin/cnd)
+		sed -i -e 's|libprotobuf-cpp-lite\.so|libprotobuf-cpp-Hlte.so|g' "${2}"
+		;;
+	vendor/bin/sensors.qcom)
+		# See commit 45e37256 for details
+		sed -i -e 's|sns\.reg|sns.los|g' "${2}"
+		sed -i -e 's|etc/sensors|vendor/snsc|g' "${2}"
+		;;
+	vendor/bin/thermal-engine)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	vendor/bin/xtra-daemon)
+		sed -i -e 's|system/priv-app|vendor/priv-app|g' "${2}"
+		;;
+	vendor/lib/hw/sound_trigger.primary.msm8952.so)
+		sed -i -e 's|system/lib|vendor/lib|g' "${2}"
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib/mediadrm/libwvdrmengine.so)
+		sed -i -e 's|libprotobuf-cpp-lite\.so|libprotobuf-cpp-Hlte.so|g' "${2}"
+		;;
+	vendor/lib/soundfx/libqcbassboost.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib/soundfx/libqcreverb.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib/soundfx/libqcvirt.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib?(64)/libacdb-fts.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib?(64)/libacdbloader.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib?(64)/libacdbmapper.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib?(64)/libacdbrtac.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib?(64)/libadiertac.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib/libadm.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib/libadpcmdec.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib?(64)/libadsprpc.so)
+		sed -i -e 's|system/lib|vendor/lib|g' "${2}"
+		;;
+	vendor/lib?(64)/libaudcal.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib?(64)/libaudioalsa.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib?(64)/libcne.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|libprotobuf-cpp-lite\.so|libprotobuf-cpp-Hlte.so|g' "${2}"
+		;;
+	vendor/lib?(64)/libcneapiclient.so)
+		sed -i -e 's|libprotobuf-cpp-lite\.so|libprotobuf-cpp-Hlte.so|g' "${2}"
+		;;
+	vendor/lib?(64)/libdpmframework.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	vendor/lib?(64)/libmm-qdcm.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	vendor/lib/liboemcrypto.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	vendor/lib?(64)/librs_adreno.so)
+		sed -i -e 's|system/lib|vendor/lib|g' "${2}"
+		;;
+	vendor/lib/libsmwrapper.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		sed -i -e 's|system/lib/hw|vendor/lib/hw|g' "${2}"
+		sed -i -e 's|etc/acdbdata|vendor/etc/a|g' "${2}"
+		;;
+	vendor/lib/libvpplibrary.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	vendor/lib64/liblbs_core.so)
+		sed -i -e 's|system/priv-app|vendor/priv-app|g' "${2}"
+		;;
+	vendor/lib64/librpmb.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	vendor/lib64/libSecureUILib.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	vendor/lib64/vendor.qti.gnss@1.0.so)
+		patchelf --replace-needed "android.hardware.gnss@1.0.so" "android.hardware.gnss@1.0-v27.so" "${2}"
+		;;
+	vendor/lib64/vendor.qti.gnss@1.0_vendor.so)
+		patchelf --replace-needed "android.hardware.gnss@1.0.so" "android.hardware.gnss@1.0-v27.so" "${2}"
+		;;
+	recovery/root/sbin/qseecomd)
+		# Inspired by bootable/recovery-twrp/prebuilt/relink.sh
+		sed -i \
+			-e "s|/system/bin/linker64\x00|/sbin/linker64\x00\x00\x00\x00\x00\x00\x00|g" \
+			-e "s|/system/bin/linker\x00|/sbin/linker\x00\x00\x00\x00\x00\x00\x00|g" \
+			-e "s|/system/bin/sh\x00|/sbin/sh\x00\x00\x00\x00\x00\x00\x00|g" \
+			-e "s|/system/lib64\x00|/sbin\x00\x00\x00\x00\x00\x00\x00\x00\x00|g" \
+			-e "s|/system/lib\x00|/sbin\x00\x00\x00\x00\x00\x00\x00|g" "${2}"
+		;;
+	vendor/bin/netmgrd)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		# Replace AID 2950 with 2901 (see commit eb1abadf)
+		sed -i -e "s|\xBC\x0B\x00\x00\xBB\x0B\x00\x00\x86\x0B\x00\x00\xE8\x03\x00\x00|\xBC\x0B\x00\x00\x55\x0B\x00\x00\x86\x0B\x00\x00\xE8\x03\x00\x00|g" "${2}"
+		;;
+	vendor/bin/qmuxd)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	vendor/lib?(64)/libdsi_netctrl.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	vendor/lib?(64)/libril-qc-qmi-1.so)
+		sed -i -e 's|system/etc|vendor/etc|g' "${2}"
+		;;
+	esac
+}
+
+# Initialize the helper for YTX703-common
+(
+	setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
+	extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
+			${KANG} --section "${SECTION}"
+	if [ -s "${MY_DIR}/proprietary-files-twrp.txt" ]; then
+		extract "${MY_DIR}/proprietary-files-twrp.txt" "${SRC}" \
+			${KANG} --section "${SECTION}"
+	fi
+)
+
+# Reinitialize the helper for YTX703-common/${device}
+(
+	setup_vendor "${DEVICE}" "${VENDOR}/${DEVICE_COMMON}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+	extract "${MY_DIR}/${DEVICE}/proprietary-files.txt" "${SRC}" \
+			${KANG} --section "${SECTION}"
+	if [ -s "${MY_DIR}/${DEVICE}/proprietary-files-twrp.txt" ]; then
+		extract "${MY_DIR}/${DEVICE}/proprietary-files-twrp.txt" "$SRC" \
+			${KANG} --section "${SECTION}"
+	fi
+)
+
+"${MY_DIR}/setup-makefiles.sh" "${CLEAN_VENDOR}"
