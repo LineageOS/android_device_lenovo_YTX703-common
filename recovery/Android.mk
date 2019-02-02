4c69e0f0b89baf9d590ce19d62ff813042df29d3
Quallenauge <Hamsi2k@freenet.de>
2019-02-02 22:22:27 +0200

    recovery: Use Android.mk instead TARGET_RECOVERY_DEVICE_DIRS.
    Repeadly build an image results on rsync errors.
    
    Change-Id: Ie1b6887bbb73da84d42cdb166b20c782e84a03b6

---
+++ recovery/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := root/init.recovery.qcom.rc
+LOCAL_MODULE_PATH       := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := twrp.fstab
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := root/etc/twrp.fstab
+LOCAL_MODULE_PATH       := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
