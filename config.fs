93285a9cc6db9b331a49babdde2b0da30af211a5
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2019-09-23 16:01:32 -0700

    config.fs: Add CAP_SYS_BOOT for charger
    Add CAP_SYS_BOOT capability for charger service so that it can
    shutdown/reboot the system.
    
    Change-Id: I17ccba74d5a3c38bd751c55bcf42fb3689d730a7

--- config.fs
+++ config.fs
@@ -35,6 +35,12 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
+[/system/bin/charger]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [/vendor/bin/sensors.qcom]
 mode: 0755
 user: AID_VENDOR_SENSORS
