ce1c367373e66e50e126e736978a27ac6846e7cc
Bruno Martins <bgcngm@gmail.com>
2019-09-30 13:32:04 +0100

    seccomp: Drop mediaextractor seccomp policy
    These syscalls are now allowed by default.
    
    Change-Id: I405f609d030331e19031612cbd912a54a3c72339

--- device-common.mk
+++ device-common.mk
@@ -133,8 +133,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/seccomp_policy/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/configs/seccomp_policy/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/configs/seccomp_policy/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Thermal
 PRODUCT_PACKAGES += \
