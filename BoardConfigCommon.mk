08ade411f39a34784f58f2867912bc30c34cd8cf
P.Adarsh Reddy <padarshr@codeaurora.org>
2020-03-09 12:16:37 +0530

    build: Add build broken flags in BoardConfig.mk
    BUILD_BROKEN_USES_BUILD_COPY_HEADERS
    
    Change-Id: I7b115f5bb7990c438c595b4b1ff536598786d529

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -25,6 +25,7 @@ include $(DEVICE_PATH)/recovery/twrp.mk
 
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 BUILD_BROKEN_DUP_RULES := true
 
 # Use Snapdragon LLVM, if available
