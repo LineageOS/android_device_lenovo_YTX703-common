2d0e592b8764ada78c38bd160ee4158834f91cc3
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    hidl: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- device-common.mk
+++ device-common.mk
@@ -287,7 +287,12 @@ PRODUCT_PACKAGES += \
 
 # Dummy hidl lib for oreo blobs
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
