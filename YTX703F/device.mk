d6efcccbd5aaa5f5d3584c26410aeb00ba2e9a80
Quallenauge <Hamsi2k@freenet.de>
2020-11-09 21:29:43 +0100

    YTX703: Inherit product_launched_with_m.mk
    Device was launched with Android 6.
    
    Change-Id: Ia25a115c25dadc68a8adf64a5f0e7b9d969dac82

--- YTX703F/device.mk
+++ YTX703F/device.mk
@@ -19,6 +19,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit from common YTX703 device
 $(call inherit-product, device/lenovo/YTX703-common/device-common.mk)
