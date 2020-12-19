b5f0022f80a271ca0a87a37d8146b2781a5d9caa
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    extract-utils: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- extract-files.sh
+++ extract-files.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 #
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
 # Load extractutils and do some sanity checks
 MY_DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
-LINEAGE_ROOT=$(readlink -f "${MY_DIR}/../../..")
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
 	echo "Unable to find helper script at $HELPER"
 	exit 1
@@ -195,7 +186,7 @@ function blob_fixup() {
 
 # Initialize the helper for YTX703-common
 (
-	setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
+	setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 	extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
 			${KANG} --section "${SECTION}"
 	if [ -s "${MY_DIR}/proprietary-files-twrp.txt" ]; then
@@ -207,7 +198,7 @@ function blob_fixup() {
 # Reinitialize the helper for YTX703-common/${device}
 (
 	source "${DEVICE}/extract-files.sh"
-	setup_vendor "${DEVICE}" "${VENDOR}/${DEVICE_COMMON}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+	setup_vendor "${DEVICE}" "${VENDOR}/${DEVICE_COMMON}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 	extract "${MY_DIR}/${DEVICE}/proprietary-files.txt" "${SRC}" \
 			${KANG} --section "${SECTION}"
 	if [ -s "${MY_DIR}/${DEVICE}/proprietary-files-twrp.txt" ]; then
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 #
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -21,10 +12,10 @@ set -e
 MY_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${MY_DIR}/../../.."
 CLEANUP="$1"
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
 	echo "Unable to find helper script at $HELPER"
 	exit 1
@@ -32,7 +23,7 @@ fi
 . "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" "true" "${CLEANUP}"
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" "true" "${CLEANUP}"
 
 # Copyright headers and guards
 write_headers "YTX703F YTX703L"
@@ -53,7 +44,7 @@ write_footers
 
 # Reinitialize the helper for YTX703-common/${device}
 (
-	setup_vendor "${DEVICE}" "${VENDOR}/${DEVICE_COMMON}" "${LINEAGE_ROOT}" "false" "${CLEANUP}"
+	setup_vendor "${DEVICE}" "${VENDOR}/${DEVICE_COMMON}" "${ANDROID_ROOT}" "false" "${CLEANUP}"
 
 	# Copyright headers and guards
 	write_headers
