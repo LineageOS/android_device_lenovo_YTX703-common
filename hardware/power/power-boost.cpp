f7c5efe33e03ee6ebdc50af3f30ece933d8b5348
Quallenauge <Hamsi2k@freenet.de>
2021-01-18 23:11:49 +0100

    power: Handle interaction boost.
    Implementation power-8952.c doesn't provide an
    interaction power hint. Instead of adding such a
    handling method to this file, we put it here
    as device specific implementation:
     - our setup is very device specific,
       since we use a newer qti perf service which is capable
       of defining boost settings in an xml file
     - msm8952 uses different indexes for bigLITTLE cluster,
       so mostly this can't be shared among these two
    
    As implementation we use the process_interaction_hint from latest
    18.1 branch ( power: Avoid reaching req limit of qti perf service. ).
    
    Change-Id: I17e8c6ca8f972d383833ff48af4449d83d55974f

--- hardware/power/power-boost.cpp
+++ hardware/power/power-boost.cpp
@@ -19,6 +19,57 @@
 
 #include <aidl/android/hardware/power/BnPower.h>
 
+extern "C" {
+#include "hint-data.h"
+#include "metadata-defs.h"
+#include "performance.h"
+#include "power-common.h"
+#include "utils.h"
+
+const int kMinInteractiveDuration = 500;  /* ms */
+const int kMaxInteractiveDuration = 5000; /* ms */
+
+static void process_interaction_hint(void *data) {
+  static struct timespec s_previous_boost_timespec;
+  static int s_previous_duration = 0;
+  static int interaction_handle = -1;
+
+  struct timespec cur_boost_timespec;
+  long long elapsed_time;
+  int duration = kMinInteractiveDuration;
+
+  if (data) {
+    int input_duration = *((int *)data);
+    if (input_duration > duration) {
+      duration = (input_duration > kMaxInteractiveDuration)
+                     ? kMaxInteractiveDuration
+                     : input_duration;
+    }
+  }
+
+  clock_gettime(CLOCK_MONOTONIC, &cur_boost_timespec);
+
+  elapsed_time =
+      calc_timespan_us(s_previous_boost_timespec, cur_boost_timespec);
+  // don't hint if it's been less than 250ms since last boost
+  // also detect if we're doing anything resembling a fling
+  // support additional boosting in case of flings
+  if (elapsed_time < 250000 && duration <= 750) {
+    return;
+  }
+  s_previous_boost_timespec = cur_boost_timespec;
+  s_previous_duration = duration;
+
+  if (CHECK_HANDLE(interaction_handle)) {
+    release_request(interaction_handle);
+    interaction_handle = -1;
+  }
+
+  interaction_handle = perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST,
+                                                  duration, SCROLL_VERTICAL);
+}
+}
+
 using ::aidl::android::hardware::power::Boost;
 
 namespace aidl {
@@ -29,6 +80,9 @@ namespace impl {
 
 bool isDeviceSpecificBoostSupported(Boost type, bool *_aidl_return) {
   switch (type) {
+  case Boost::INTERACTION:
+    *_aidl_return = true;
+    return true;
   default:
     return false;
   }
@@ -36,6 +90,9 @@ bool isDeviceSpecificBoostSupported(Boost type, bool *_aidl_return) {
 
 bool setDeviceSpecificBoost(Boost type, int32_t durationMs) {
   switch (type) {
+  case Boost::INTERACTION:
+    process_interaction_hint(&durationMs);
+    return true;
   default:
     return false;
   }
